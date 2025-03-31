5449d04b2bcf4e0c1e51a3986cf6b1993266fd42
Nathan Sala <sala.nathan@gmail.com>
2025-03-31 11:56:12 +0900

    use isDeepStrictEqual instead of lodash.isequal, update dependencies (#7)

--- eslint.config.js
+++ eslint.config.js
@@ -12,10 +12,10 @@ module.exports = [
       },
     },
     rules: {
-      'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+      'no-unused-vars': ['error', {argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_'}],
       'no-multi-spaces': ['error', {exceptions: {VariableDeclarator: true}}],
       'no-else-return': ['error', {allowElseIf: true}],
-      'max-params': ['error', 6],
+      'max-params': ['error', 7],
       'no-await-in-loop': 'off',
       'max-len': ['error', {code: 150}],
       'mocha/no-identical-title': 'error',
@@ -26,6 +26,6 @@ module.exports = [
       'mocha/no-top-level-hooks': 'off',
       'mocha/no-hooks-for-single-case': 'off',
     },
-    ignores: ['node_modules/'],
+    ignores: ['node_modules/*', 'artifacts*/**', 'cache*/**', 'imports/*', 'coverage*/**', 'flattened/*'],
   },
 ];
